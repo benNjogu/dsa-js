f9228a054a55ca106fada3dcb839eec34cfabcb9
benNjogu <kamandemachafi@gmail.com>
2023-03-24 16:37:36 +0300

    Stack theory

---
+++ stack/index.js
@@ -0,0 +1,11 @@
+/**
+ * @stack
+ * It is a LIFO data structure.
+ * e.g a pile of books, a pile of plates etc
+ *
+ * it has four main methods:
+ *  push - used to add elements to the stack
+ *  pop - used to remove an element from the stack
+ *  peek - used to get the very top most element in the stack
+ *  length/size - used to determine the number of element in the stack
+ */
