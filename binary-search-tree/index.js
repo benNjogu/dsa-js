7b48094c03c495166b5e032f78a00d1afb133496
benNjogu <kamandemachafi@gmail.com>
2023-03-26 17:38:06 +0300

    Binary search tree

---
+++ binary-search-tree/index.js
@@ -0,0 +1,14 @@
+/**
+ * @Tree
+ * all data points in a tree are called nodes.
+ * the top of the tree is called the root node.
+ * the node with branches leading to other nodes is reffered to as the parent node.
+ * leaf-nodes are nodes at the end of the tree that have no branches.
+ *
+ * @binary_tree
+ * can only have two branches at single node.
+ * are ordered: each subtree is less or greater than the parent node.
+ * each right subtree is greater than or equal to the parent node.
+ *
+ * faster than searching an array but slower than searching a hashtable.
+ */
