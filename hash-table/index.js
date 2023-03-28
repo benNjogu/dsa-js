5f5fae673a959d7138aab3caa6cd734cd3f510f5
benNjogu <kamandemachafi@gmail.com>
2023-03-28 23:37:59 +0300

    Implementing a HashTable

---
+++ hash-table/index.js
@@ -0,0 +1,8 @@
+/**
+ * @hash_table
+ * Used to implement associative arrays or mapping key value pairs
+ * common way to implement the map DS or objects.
+ * widely used because of their efficiency.
+ *  it searches, inserts and deletes in constant time in best case scenario and linear time in worst case scenario
+ *
+ */
