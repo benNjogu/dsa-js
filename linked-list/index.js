1791eaad2f6f35c9abef2b7c6394e77e70d2a5cf
benNjogu <kamandemachafi@gmail.com>
2023-03-30 00:35:24 +0300

    LinkedLists

---
+++ linked-list/index.js
@@ -0,0 +1,8 @@
+/**
+ * a data structure where elements are stored in a node.
+ * the node contains two key pieces of information:-
+ *      - the element and
+ *      - the reference to the next node.
+ *
+ *
+ */
