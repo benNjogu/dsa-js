f62a05a7c28871170106c872ec16e81a3f8ccf25
benNjogu <kamandemachafi@gmail.com>
2023-03-30 10:09:11 +0300

    Trie

---
+++ trie/index.js
@@ -0,0 +1,8 @@
+/**
+ * @Trie
+ * a special type of tree used to store associative data structures.
+ * it stores data in steps. each step is a node in a trie.
+ * it is often used to store words since there are a finite number of letters that can be put together to make a string.
+ *
+ * can be used to validate that a word is in a dictionary. each step would rep one letter of a word.
+ */
